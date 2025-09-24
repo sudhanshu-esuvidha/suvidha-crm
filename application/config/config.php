62685c672a4230d6e37486c9080b79771dec93ed
harshita-esuivdha <esuvidha.intern@gmail.com>
2025-09-24 15:35:05 +0530

    conf and data

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 date_default_timezone_set("Asia/Kolkata");
-// $config['base_url'] = 'https://esuvidhatech.in/suvidha-crm/';
-$config['base_url'] = 'http://localhost/suvidha-crm/';
+$config['base_url'] = 'https://esuvidhatech.in/suvidha-crm/';
+// $config['base_url'] = 'http://localhost/suvidha-crm/';
 
 /*
 |--------------------------------------------------------------------------
